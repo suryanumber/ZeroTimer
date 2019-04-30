8a85adf7f33794814a4352833ec2612e03669663
MHG <yousukoprunt34@gmail.com>
2019-04-30 10:10:27 +0900

    Initial commit

---
+++ app/src/main/java/com/gam/hin/zerotimer/MainActivity.java
@@ -0,0 +1,114 @@
+package com.gam.hin.zerotimer;
+
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final long START_TIME_IN_MILLIS = 600000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // --画面--
+        // 設定画面・・・タイマーをセットする、タイマーの時間と、休憩時間、インターバル数。
+        // タイマー画面・・・タイマーの時間（１秒ごとに画面を再描画し、時間の経過がわかるように）
+        //                設定画面を呼び出すボタン、開始ボタン、停止ボタン、一時停止ボタン
+        // --機能--
+        // タイマーの時間が終了したら、音を鳴らす。
+        // インターバル数を確認し、0でなければ休憩時間を起動する
+        // その際、インターバル数のカウントをマイナス1する。
+        // 休憩時間が終了したら音を鳴らす。
+        // タイマーを起動する
+        // タイマーの時間が終了して、インターバル数が0だったら終了する。
+        // 一時停止
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning){
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                resetTimer();
+            }
+
+        });
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+}
